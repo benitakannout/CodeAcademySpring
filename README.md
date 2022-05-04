43459df856f7f08efd452e8dcb21df3418d4531a
benitakannout <94390863+benitakannout@users.noreply.github.com>
2022-05-04 14:22:42 +0300

    Project created

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CodeAcademySpring
+
 CodeAcademy spring project
---
+++ src/test/java/eu/codeacademy/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.codeacademy.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
