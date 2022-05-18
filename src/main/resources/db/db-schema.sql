a6ceb9e1ea38245e3c4f9df51b9935d262e9b483
benitakannout <94390863+benitakannout@users.noreply.github.com>
2022-05-18 14:03:05 +0300

    SQL files updated with initial data

---
+++ src/main/resources/db/db-schema.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS daily_questions;
+
+CREATE TABLE daily_questions (
+                         id SERIAL PRIMARY KEY NOT NULL,
+                         question VARCHAR(500) NOT NULL
+);
+
+DROP TABLE IF EXISTS public_questions;
+
+CREATE TABLE public_questions (
+                        id SERIAL PRIMARY KEY NOT NULL,
+                        date DATE NOT NULL,
+                        question VARCHAR(500) NOT NULL
+);
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+                        id SERIAL PRIMARY KEY NOT NULL,
+                        username VARCHAR(50) NOT NULL,
+                        name VARCHAR(15) NOT NULL,
+                        email VARCHAR(60) NOT NULL,
+                        city VARCHAR(15),
+                        password VARCHAR(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS answer_card;
+
+CREATE TABLE answer_card (
+                        id SERIAL PRIMARY KEY NOT NULL,
+                        user_id SERIAL REFERENCES users(id),
+                        day_number INT NOT NULL,
+                        difficulty INT NOT NULL,
+                        fulfillment INT NOT NULL,
+                        motivation INT NOT NULL,
+                        question SERIAL REFERENCES daily_questions(id),
+                        answer VARCHAR(600)
+);
---
+++ src/main/resources/db/initial-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO daily_questions (question)
+VALUES ('Write a motivational mantra to repeat each day.'),
+       ('Why is this habit a priority for me?'),
+       ('What one word do I associate with my habit? Reflect on it.'),
+       ('How will this new habit improve my daily life?'),
+       ('What is my ultimate "why"?'),
+       ('What time of the day is easiest for me to commit to my new habit?'),
+       ('Write a motivational note from the perspective of your future self'),
+       ('What was the main thing blocking me from adopting this habit in the past?'),
+       ('What one thing can I do to make my habit more fun?'),
+       ('What is the most rewarding thing about committing to my habit?'),
+       ('How does my new habit make me a better person?')
