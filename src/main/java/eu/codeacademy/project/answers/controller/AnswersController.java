b76dbb30b25eb9bc198cc4df72b5cee7b56cfc47
benitakannout <94390863+benitakannout@users.noreply.github.com>
2022-05-31 13:47:37 +0300

    entry form created

--- src/main/java/eu/codeacademy/project/answers/controller/AnswersController.java
+++ src/main/java/eu/codeacademy/project/answers/controller/AnswersController.java
@@ -2,14 +2,24 @@ package eu.codeacademy.project.answers.controller;
 
 
 import eu.codeacademy.project.answers.dto.AnswersDto;
+import eu.codeacademy.project.answers.entity.Answers;
 import eu.codeacademy.project.answers.service.AnswersService;
+import eu.codeacademy.project.questions.repository.QuestionsRepository;
+import eu.codeacademy.project.questions.service.QuestionsService;
+import eu.codeacademy.project.user.entity.User;
+import eu.codeacademy.project.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServer;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.Optional;
 
 @Controller
@@ -18,6 +28,8 @@ import java.util.Optional;
 public class AnswersController {
 
     private final AnswersService answersService;
+    private final UserService userService;
+    private final QuestionsService questionsService;
 
     @GetMapping("/answers/{questionId}/update")
     public String getUserAnswers(Model model, @PathVariable("questionId") int id) {
@@ -27,6 +39,22 @@ public class AnswersController {
             return "/answers/answers";
         }
 
-        return "/questions/questions";
+        return "redirect:/answers/open";
     }
+
+    @GetMapping("/answers/open")
+    public String openCreateAnswer(Model model) {
+        model.addAttribute("answer", AnswersDto.builder().build());
+
+        return "/answers/create";
+    }
+
+
+    @PostMapping("/answers/create")
+    public String createAnswer(Model model, AnswersDto answer) {
+        model.addAttribute("answer", answer);
+        answersService.createAnswer(answer);
+        return "redirect:questions/questions";
+    }
+
 }
--- src/main/java/eu/codeacademy/project/answers/service/AnswersService.java
+++ src/main/java/eu/codeacademy/project/answers/service/AnswersService.java
@@ -4,10 +4,12 @@ import eu.codeacademy.project.answers.dto.AnswersDto;
 import eu.codeacademy.project.answers.entity.Answers;
 import eu.codeacademy.project.answers.mapper.AnswersMapper;
 import eu.codeacademy.project.answers.repository.AnswersRepository;
+import eu.codeacademy.project.questions.repository.QuestionsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,18 +22,23 @@ public class AnswersService {
     private final AnswersRepository answersRepository;
     private final AnswersMapper mapper;
 
-    public List<AnswersDto> getAnswers() {
-        return answersRepository.findAll().stream()
-                .map(mapper::mapTo)
-                .collect(Collectors.toList());
-    }
-
     public Optional<AnswersDto> getAnswerByUserAndDay(int dayNumber) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentPrincipalName = authentication.getName();
         Optional<Answers> answer = answersRepository.findAnswersByUsernameAndDayNumber(currentPrincipalName, dayNumber);
         Optional<AnswersDto> answerMapped = answer.map(mapper::mapTo);
         return answerMapped;
+    }
+
+    @Transactional
+    public void createAnswer(AnswersDto answer) {
+
+        answersRepository.save(Answers.builder()
+                        .difficulty(answer.getDifficulty())
+                        .fulfillment(answer.getFulfillment())
+                        .motivation(answer.getMotivation())
+                        .answer(answer.getAnswer())
+                .build());
     }
 
 }
