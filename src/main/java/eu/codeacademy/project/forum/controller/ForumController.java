cb6f52daba245fff52ee8c1be99fbd6bf8c14daa
benitakannout <94390863+benitakannout@users.noreply.github.com>
2022-06-07 06:06:23 +0300

    controller and service created, forum main page with questions done

--- src/main/java/eu/codeacademy/project/forum/controller/ForumController.java
+++ src/main/java/eu/codeacademy/project/forum/controller/ForumController.java
@@ -1,7 +1,24 @@
 package eu.codeacademy.project.forum.controller;
 
+import eu.codeacademy.project.forum.service.ForumService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/forum")
 public class ForumController {
+
+    private final ForumService forumService;
+
+    @GetMapping
+    public String getPublicQuestions (Model model) {
+        model.addAttribute("questions", forumService.getPublicQuestions());
+
+        return "forum/questions";
+    }
+
 }
--- src/main/java/eu/codeacademy/project/forum/service/ForumService.java
+++ src/main/java/eu/codeacademy/project/forum/service/ForumService.java
@@ -1,7 +1,24 @@
 package eu.codeacademy.project.forum.service;
 
+import eu.codeacademy.project.forum.dto.PublicQuestionDto;
+import eu.codeacademy.project.forum.mapper.PublicQuestionMapper;
+import eu.codeacademy.project.forum.repository.PublicQuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class ForumService {
+
+    private final PublicQuestionRepository publicQuestionRepository;
+    private final PublicQuestionMapper questionMapper;
+
+    public List<PublicQuestionDto> getPublicQuestions() {
+        return publicQuestionRepository.findAll().stream()
+                .map(questionMapper::mapTo)
+                .collect(Collectors.toList());
+    }
 }
